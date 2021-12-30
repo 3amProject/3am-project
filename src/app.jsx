3efe5723f4f8b6d1e2522231abbb568d31cb4b78
kimmsoll <kimmsoll@naver.com>
2021-12-31 00:03:33 +0900

    feat: 회원 로그인 페이지 생성

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home";
+import LoginPage from "./routes/login-page/login-page";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route exact path="/" element={<Home/>} />
+      <Route path="/login" element={<LoginPage/>} />
     </Routes>
     </BrowserRouter>
   );
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,13 +3,13 @@ import styles from './footer.module.css';
 function Footer(){
     return (
         <footer className={styles.footer}>
-            <button>
+            <button className={styles.button}>
                 <i className="fas fa-bars"></i>
             </button>
-            <button>
+            <button className={styles.button}>
                 <i className="far fa-calendar-check"></i>
             </button>
-            <button>
+            <button className={styles.button}>
                 <i className="fas fa-user"></i>
             </button>
         </footer>
