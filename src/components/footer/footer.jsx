8899c1837a37e692f25d31c99ea8a048dbf10a41
kimmsoll <kimmsoll@naver.com>
2021-12-31 16:19:16 +0900

    feat: footer 링크 생성

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from './footer.module.css';
 
 const Footer = ({onClick}) => {
@@ -6,12 +7,16 @@ const Footer = ({onClick}) => {
             <button className={styles.button} onClick={onClick}>
                 <i className="fas fa-bars"></i>
             </button>
+            <Link to="/">
             <button className={styles.button}>
                 <i className="far fa-calendar-check"></i>
             </button>
+            </Link>
+            <Link to="/profile">
             <button className={styles.button}>
                 <i className="fas fa-user"></i>
             </button>
+            </Link>
         </footer>
     )
 };
