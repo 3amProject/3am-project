330b74e8f4052db7bf2c4287666f04c03c7cd2ac
kimmsoll <kimmsoll@naver.com>
2021-12-31 16:24:18 +0900

    회원 가입, 마이 페이지 링크 구현

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home";
+import JoinPage from "./routes/joinPage/joinPage";
 import LoginPage from "./routes/loginPage/loginPage";
+import Profile from "./routes/profile/profile";
 
 function App() {
+  const [isOpen, setState] = useState(false);
+    const toggleSideBar = () => {
+        setState(isOpen => !isOpen);
+    }
   return (
     <BrowserRouter>
     <Routes>
-      <Route exact path="/" element={<Home/>} />
+      <Route exact path="/" element={<Home
+        onClick={toggleSideBar}
+        isOpen={isOpen}
+      />} />
+      <Route path="/join" element={<JoinPage
+        onClick={toggleSideBar}
+        isOpen={isOpen}
+      />} />
       <Route path="/login" element={<LoginPage/>} />
+      <Route path="/profile" element={<Profile/>} />
     </Routes>
     </BrowserRouter>
   );
---
+++ src/routes/joinPage/joinPage.jsx
@@ -0,0 +1,22 @@
+import Footer from '../../components/footer/footer';
+import Header from '../../components/header/header';
+import Join from '../../components/join/join';
+import SideBar from '../../components/sideBar/sideBar';
+import styles from './joinPage.module.css';
+
+const JoinPage = ({onClick, isOpen}) => {
+    return(
+        <>
+        <section className={styles.joinPage}>
+            <Header></Header>
+            <main className={styles.main}>
+            <Join></Join>
+            </main>
+            <Footer onClick={onClick} isOpen={isOpen}></Footer>
+        <SideBar onClick={onClick} isOpen={isOpen}></SideBar>
+        </section>
+        </>
+    );
+}
+
+export default JoinPage;
