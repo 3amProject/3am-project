41d3002ce57733f1a0b7f5aecdba16d794a4e255
kimmsoll <kimmsoll@naver.com>
2021-12-31 16:22:15 +0900

    feat: 회원가입 페이지 구현

--- src/components/join/join.jsx
+++ src/components/join/join.jsx
@@ -1,8 +1,9 @@
 import styles from './join.module.css';
+import Button from '../button/button';
 
 const Join = () => {
     return (
-        <>
+        <section className={styles.join}>
         <h1 className={styles.title}>회원 가입</h1>
         <div className={styles.inputs}>
         <input className={styles.input} type="text" placeholder="아이디" />
@@ -14,9 +15,10 @@ const Join = () => {
         </div>
         <div className={styles.buttons}>
             <Button text="취소"></Button>
+            <p></p>
             <Button text="회원 가입"></Button>
         </div>
-        </>
+        </section>
     );
 }
 
