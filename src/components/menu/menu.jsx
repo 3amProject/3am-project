bab2b2a7ab456abdeb798ce74421a6ae814720fe
kimmsoll <kimmsoll@naver.com>
2022-01-08 14:41:50 +0900

    feat: showPrice 함수 추가

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,7 +1,11 @@
 import styles from './menu.module.css';
 
 const Menu = ({menu}) => {
-    console.log(menu);
+    const showPrice = (price) => {
+        let newPrice = String(price).split('');
+        newPrice.splice(2,0,',');
+        return newPrice.join('') + '원';
+    }
     return (
         <div className={styles.menu}>
             <div className={styles.imgContainer}>
@@ -9,7 +13,7 @@ const Menu = ({menu}) => {
             </div>
             <p className={styles.name}>{menu.name}</p>
             <div className={styles.footer}>
-                <span className={styles.price}>{menu.price}</span>
+                <span className={styles.price}>{showPrice(menu.price)}</span>
                 <button className={styles.cartBtn}>
                     <i className="fas fa-shopping-basket"></i>
                 </button>
