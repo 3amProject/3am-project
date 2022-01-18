5b7ee832d112aeadadf7dffb6abab47d983ae223
kimmsoll <kimmsoll@naver.com>
2022-01-18 17:37:38 +0900

    feat: 비회원 로그인 화면 생성

---
+++ src/components/notUserLogin.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from './button';
+
+const DivComponent = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 2em 0em 1em 0em;
+    & input {
+        height: 2em;
+        margin-bottom: 0.5em;          
+    }
+`;
+
+const NotUserLogin = () => {
+    return (
+        <DivComponent>
+        <input type="text" placeholder=" 주문번호"/>
+        <input type="password" placeholder=" 비밀번호"/>
+        <Button text="확인"></Button>
+        </DivComponent>
+    );
+}
+
+export default NotUserLogin;
--- src/routes/loginPage.jsx
+++ src/routes/loginPage.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import Button from '../components/button';
+import NotUserLogin from '../components/notUserLogin';
+import UserLogin from '../components/userLogin';
 
 const DivComponent = styled.div`
     width: 100vw;
@@ -48,48 +49,21 @@ const HeaderComponent = styled.header`
     }
 `;
 const MainComponent = styled.main`
-    display: flex;
-    flex-direction: column;
     width: 80%;
-    margin: 2em 0em 1em 0em;
-    & input {
-        height: 2em;
-        margin-bottom: 0.5em;          
-    }
-    & form {
-        display: flex;
-        justify-content: space-between;
-        margin-top: 1em;
-        & div {
-            display: flex;
-            align-items: center;
-        }
-    }
+    height: 50%;
 `;
-const FooterComponent = styled.footer`
-    width: 80%;
-    display: flex;
-    flex-direction: column;
-    & div {
-        display: flex;
-        justify-content: center;
-        margin: 0.5em 0em;
-        & span {
-            border-right: 1px solid black;
-        }
-        & button {
-            background-color: transparent;
-            border: none;
-            &: hover {
-                cursor: pointer;
-            }
-        }
-    }
-`;
-
-
 const LoginPage = () => {
+    const [isUser, setIsUser] = useState(true);
+
     // security api 로 로그인 화면 조회해야 함
+    const handleShow = (e) => {
+        if(e.target.textContent === '회원'){
+            setIsUser(true);
+        }else{
+            setIsUser(false);
+        }
+    }
+
     return (
         <DivComponent>
         <SectionComponent>
@@ -101,35 +75,13 @@ const LoginPage = () => {
                 </Link>
                 <h1>로그인</h1>
                 <div>
-                    <button>회원</button>
-                    <button>비회원</button>
+                    <button onClick={handleShow}>회원</button>
+                    <button onClick={handleShow}>비회원</button>
                 </div>
             </HeaderComponent>
             <MainComponent>
-                <input type="text" placeholder=" 아이디"/>
-                <input type="password" placeholder=" 비밀번호"/>
-                <form>
-                    <div>
-                    <input type="checkbox" name="checkId"/>
-                    <label htmlFor="checkId">아이디 저장</label>
-                    </div>
-                    <div>
-                    <input type="checkbox" name="checkLogin"/>
-                    <label htmlFor="checkLogin">자동 로그인</label>
-                    </div>
-                </form>
+            {isUser ? <UserLogin/> : <NotUserLogin/>}
             </MainComponent>
-            <FooterComponent>
-                <Button text="로그인"></Button>
-                <div>
-                    <button>아이디 찾기</button>
-                    <span></span>
-                    <button>비밀번호 찾기</button>
-                </div>
-                <Link to='/join'>
-                    <Button text="회원 가입"></Button>
-                </Link>
-            </FooterComponent>
         </SectionComponent>
         </DivComponent>
     )
